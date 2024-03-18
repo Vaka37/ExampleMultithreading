f2c5573a909b678de81d6f87e1350e983b1b6042
Vakil <>
2024-03-18 16:51:39 +0300

    app init

---
+++ Multithreading/Multithreading/FirstViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Multithreading
+//
+//  Created by Vakil on 18.03.2024.
+//
+
+import UIKit
+/// Стартовый контроллер приложения
+final class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red 
+    }
+}
+
